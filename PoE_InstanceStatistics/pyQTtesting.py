c52406d96ff070a6645d23edfcb5818c7b1b2349
Christoffer <christoffer.antonsen@gmail.com>
2018-05-27 17:00:30 +0200

    .

--- PoE_InstanceStatistics/pyQTtesting.py
+++ PoE_InstanceStatistics/pyQTtesting.py
@@ -1,6 +1,8 @@
 from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox, QWidget, QAction, QTabWidget,QVBoxLayout
 from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
 import sys
 
 class Window(QMainWindow):
@@ -18,7 +20,12 @@ class Window(QMainWindow):
         #button.clicked.connect(self.close)
         button.clicked.connect(self.CloseApp)
         
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+        
         self.InitWindow()
+        
+        
         
     def InitWindow(self):
         self.setWindowTitle(self.title)
@@ -32,6 +39,39 @@ class Window(QMainWindow):
         reply = QMessageBox.question(self, "Close message", "Exit the application?", QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
             self.close()
+
+class MyTableWidget(QWidget):        
+ 
+    def __init__(self, parent):   
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+ 
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()	
+        self.tab2 = QWidget()
+        self.tabs.resize(300,200) 
+ 
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab2,"Tab 2")
+ 
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.setLayout(self.tab1.layout)
+ 
+        # Add tabs to widget        
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+ 
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+ 
 
 #App = QApplication(sys.argv)
 App = QCoreApplication.instance()
