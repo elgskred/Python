00c175640e23276f666d758bea01f8c7507ece9d
elgskred <christoffer.antonsen@gmail.com>
2018-06-09 16:58:12 +0200

    .

--- PoE_InstanceStatistics/main.py
+++ PoE_InstanceStatistics/main.py
@@ -232,67 +232,67 @@ df = pd.DataFrame(columns=columns)
 t = dataStruct['lastOpened']
 dataStruct['lastOpened'] = t
 
-while True:
-    
-    dataStruct = OpenDataStructure()
-    logData = iterateLogfile(dataStruct['lastOpened'])
-    print(countOpenings(logData))
-    #First time the logger is run. If PoE is running, find the latest ** log file opening ** and go from there.
-    if dataStruct['lastOpened'] == defaultDateTime:
-        print('First time exe is run.')
-        if CheckIfRunning():
-            print('PoE is running')
-            logData = iterateLogfile(dataStruct['lastOpened'])
-            dataStruct['lastOpened'] = logData[-1].split()[0:2]
-            df = clearDF()
-            df = df.append(pd.DataFrame(parseLogfile(logData)))
-            
-            lastLength = len(logData)
-        #If PoE is not running. Save the timestamp of last ** log file opening **.
-        else:
-            print('PoE is not running')
-            logData = iterateLogfile(dataStruct['lastOpened'])
-            dataStruct['lastOpened'] = logData[-1].split()[0:2]
-            df = clearDF()
-            SaveDataStructure(dataStruct)
-            lastLength = len(logData)
-    #If countOpenings = 1, means PoE is either running or has not been started since last check, should either expect a new log opened or parse new lines.
-    #If countOpenings is greater than 1, means PoE has been opened at least once since last check, parse new info and save it to our datastruct.
-    elif len(countOpenings(logData)) > 1:
-        print('lastOpened has been updated. Parse new data.')
-        if CheckIfRunning():
-            print('PoE is running')
-            occurences = countOpenings(logData)
-            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
-            df = clearDF()
-            df = df.append(pd.DataFrame(parseLogfile(logData[occurences[1]:occurences[-2]])))
-            saveToTotalstats(df,dataStruct)
-            df = clearDF()
-            df = df.append(pd.DataFrame(parseLogfile(logData[0:occurences[1]])))
-            saveToSession(df, dataStruct)
-            lastLength = len(logData)
-        else:
-            print('PoE is not running')
-            occurences = countOpenings(logData)
-            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
-            df = clearDF()
-            df = df.append(pd.DataFrame(parseLogfile(logData[0:occurences[-2]])))
-            saveToTotalstats(df, dataStruct)
-            lastLength = len(logData)
-    #If logdata length has increased, but no new log file opened has beeen found, meaning poe is runnning and there is new info to parse.
-    elif len(logData) > lastLength:
-        print('New lines to parse')
-        if CheckIfRunning():
-            print('PoE is running')
-            occurences = countOpenings(logData)
-            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
-            df = df.append(pd.DataFrame(parseLogfile(logData[0:(len(logData)-lastLength+1)])))
-            saveToSession(df, dataStruct)
-            lastLength = len(logData)
-    else:
-        tm.sleep(10)
-        print('Sleep')
-    print(dataStruct)
+#while True:
+#    
+#    dataStruct = OpenDataStructure()
+#    logData = iterateLogfile(dataStruct['lastOpened'])
+#    print(countOpenings(logData))
+#    #First time the logger is run. If PoE is running, find the latest ** log file opening ** and go from there.
+#    if dataStruct['lastOpened'] == defaultDateTime:
+#        print('First time exe is run.')
+#        if CheckIfRunning():
+#            print('PoE is running')
+#            logData = iterateLogfile(dataStruct['lastOpened'])
+#            dataStruct['lastOpened'] = logData[-1].split()[0:2]
+#            df = clearDF()
+#            df = df.append(pd.DataFrame(parseLogfile(logData)))
+#            
+#            lastLength = len(logData)
+#        #If PoE is not running. Save the timestamp of last ** log file opening **.
+#        else:
+#            print('PoE is not running')
+#            logData = iterateLogfile(dataStruct['lastOpened'])
+#            dataStruct['lastOpened'] = logData[-1].split()[0:2]
+#            df = clearDF()
+#            SaveDataStructure(dataStruct)
+#            lastLength = len(logData)
+#    #If countOpenings = 1, means PoE is either running or has not been started since last check, should either expect a new log opened or parse new lines.
+#    #If countOpenings is greater than 1, means PoE has been opened at least once since last check, parse new info and save it to our datastruct.
+#    elif len(countOpenings(logData)) > 1:
+#        print('lastOpened has been updated. Parse new data.')
+#        if CheckIfRunning():
+#            print('PoE is running')
+#            occurences = countOpenings(logData)
+#            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
+#            df = clearDF()
+#            df = df.append(pd.DataFrame(parseLogfile(logData[occurences[1]:occurences[-2]])))
+#            saveToTotalstats(df,dataStruct)
+#            df = clearDF()
+#            df = df.append(pd.DataFrame(parseLogfile(logData[0:occurences[1]])))
+#            saveToSession(df, dataStruct)
+#            lastLength = len(logData)
+#        else:
+#            print('PoE is not running')
+#            occurences = countOpenings(logData)
+#            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
+#            df = clearDF()
+#            df = df.append(pd.DataFrame(parseLogfile(logData[0:occurences[-2]])))
+#            saveToTotalstats(df, dataStruct)
+#            lastLength = len(logData)
+#    #If logdata length has increased, but no new log file opened has beeen found, meaning poe is runnning and there is new info to parse.
+#    elif len(logData) > lastLength:
+#        print('New lines to parse')
+#        if CheckIfRunning():
+#            print('PoE is running')
+#            occurences = countOpenings(logData)
+#            dataStruct['lastOpened'] = logData[occurences[0]].split()[0:2]
+#            df = df.append(pd.DataFrame(parseLogfile(logData[0:(len(logData)-lastLength+1)])))
+#            saveToSession(df, dataStruct)
+#            lastLength = len(logData)
+#    else:
+#        tm.sleep(10)
+#        print('Sleep')
+#    print(dataStruct)
             
 
  
@@ -303,87 +303,7 @@ while True:
     
     
     
-#while True:
-#    
-#    logData = iterateLogfile(dataStruct['lastOpened'])
-#    logDataLength = len(logData)
-#    #Log file opening should be the last entry in the list, if not run through the entire list to find it.
-#    if '***** LOG FILE OPENING *****' in logData[-1]:
-#        tempString = logData[-1]
-#    else:
-#        for lines in logData:
-#            if '***** LOG FILE OPENING *****' in lines:
-#                tempString = lines
-#    #Getting the date of the last time PathOfExile was started and storing it as a datetime object
-#    lastOpened = tempString.split()[0:2]
-#    lastOpened = lastOpened[0] + ',' + lastOpened[1]
-#    dateTime_object = datetime.strptime(lastOpened, '%Y/%m/%d,%H:%M:%S')
-#    
-#    #If lastOpened == default, means this is the first time the script is run. Save the last opening of path
-#    if dataStruct['lastOpened'] == defaultDateTime:
-#        dataStruct['lastOpened'] = dateTime_object
-#        print('lastOpened set')
-#        #Parse log data
-#        df = clearDF()
-#        df = df.append(pd.DataFrame(parseLogfile(logData)))
-#        lastLength = logDataLength
-#    #If lastOpened is unequal to the last time the variable was checked, save it and the length of the new logs - Means a new session is started
-#    elif (dataStruct['lastOpened'] != dateTime_object):
-#        dataStruct['lastOpened'] = dateTime_object
-#        print('lastOpened updated')
-#        df = clearDF()
-#        df = df.append(pd.DataFrame(parseLogfile(logData)))
-#        lastLength = logDataLength
-#    #If lastOpened is equal to the datetime object, check to see if there are any new lines added to the logfile.
-#    else:
-#        if lastLength < logDataLength:
-#            print('New lines added to logfile')
-#            #Append new instance events to dataframe
-#            df = df.append(pd.DataFrame(parseLogfile(logData[0:(logDataLength-lastLength+1)])))
-#            lastLength = logDataLength
-#        else:
-#            clearDF()
-#    
-#    #Get the timeDelta for the instance events
-#    df['timeDelta'] = df['time'].shift(-1) - df['time']
-#    df['timeDelta'] = df['timeDelta'].apply(lambda x: x.seconds)
-#
-#    #Calculate the session length
-#    sessionLength = datetime.now() - dataStruct['lastOpened']
-#    instances = {}
-#    
-#    #Instantiate/reset keyvalue pairs for dictionary
-#    for index, row in df.iterrows():
-#        if row.instance not in instances:
-#            instances[row.instance] = {}
-#            instances[row.instance]['count'] = 0
-#            instances[row.instance]['avgTime'] = 0
-#            instances[row.instance]['avgTimeCounter'] = 0
-#            
-#    #Loop through and count all the instance events    
-#    for index, row in df.iterrows():
-#        if row.instance in instances:
-#            instances[row.instance]['count'] += 1
-#            if row.timeDelta > 0:
-#                instances[row.instance]['avgTime'] += row.timeDelta
-#                instances[row.instance]['avgTimeCounter'] += 1
-#    #Calculate the avg time spent in each instance
-#    for s in instances.keys():
-#        if instances[s]['avgTime'] > 0:
-#            instances[s]['avgTime'] = int(instances[s]['avgTime'] / instances[s]['avgTimeCounter'])
-#        else:
-#            instances[s]['avgTime'] = 0
-#        instances[s]['avgTime'] = divmod(instances[s]['avgTime'], 60)
-#        instances[s].pop('avgTimeCounter')
-#        
-#    #Save session statistics
-#    instances['sessionLength'] = sessionLength
-#    dataStruct['sessionStats'] = instances
-#    SaveDataStructure(dataStruct)
-#    
-#    
-#
-#print(dataStruct['totalStats']["Lioneye's Watch"]) 
+
     
     
     
--- PoE_InstanceStatistics/pyQTtesting.py
+++ PoE_InstanceStatistics/pyQTtesting.py
@@ -1,8 +1,10 @@
-from PyQt5 import QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox, QWidget, QAction, QTabWidget,QVBoxLayout
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QToolTip, QMessageBox, QWidget, QAction, QTabWidget,QVBoxLayout, QBoxLayout, QFormLayout, QLabel, QScrollArea, QScrollBar, QInputDialog, QLineEdit, QFileDialog, QGridLayout
+from PyQt5.QtCore import QCoreApplication, QRect, pyqtSlot
+from PyQt5.QtGui import QIcon, QFont
+import main
+import operator
+
+
 import sys
 
 class Window(QMainWindow):
@@ -15,14 +17,10 @@ class Window(QMainWindow):
         self.width = 680
         self.height = 500
         
-        button = QPushButton('Close', self)
-        button.move(200,200)
-        #button.clicked.connect(self.close)
-        button.clicked.connect(self.CloseApp)
-        
-        self.table_widget = MyTableWidget(self)
-        self.setCentralWidget(self.table_widget)
-        
+        #button = QPushButton('Close', self)
+        #button.move(400,100)
+        #button.clicked.connect(self.CloseApp)
+
         self.InitWindow()
         
         
@@ -30,6 +28,12 @@ class Window(QMainWindow):
     def InitWindow(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.top, self.left, self.width, self.height)
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+        
+        self.quit = QAction("Quit", self)
+        self.quit.triggered.connect(self.closeEvent)
+        
         self.show()
         
     def Close_Clicked(self):
@@ -39,39 +43,179 @@ class Window(QMainWindow):
         reply = QMessageBox.question(self, "Close message", "Exit the application?", QMessageBox.Yes | QMessageBox.No)
         if reply == QMessageBox.Yes:
             self.close()
+    
+    def closeEvent(self, event):
+        print("Close")
 
 class MyTableWidget(QWidget):        
  
     def __init__(self, parent):   
         super(QWidget, self).__init__(parent)
         self.layout = QVBoxLayout(self)
+        
+        #Fonts
+        boldFont = QFont()
+        boldFont.setBold(True)
  
         # Initialize tab screen
         self.tabs = QTabWidget()
-        self.tab1 = QWidget()	
-        self.tab2 = QWidget()
-        self.tabs.resize(300,200) 
- 
+        self.tab1 = QWidget()
+        self.tab2 = QScrollArea()
+        self.tab3 = QWidget()
+
         # Add tabs
-        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab1,"Statistics")
         self.tabs.addTab(self.tab2,"Tab 2")
+        self.tabs.addTab(self.tab3, "Settings")
  
+        #Create Statistics tab with content
+        self.tab1.layout = QGridLayout(self)
+        if not main.CheckIfSaveExists():
+            if main.CreateEmptyDataStructure():
+                print('Savefile created')
+                self.dataStruct = main.OpenDataStructure()
+            else:
+                print('Something went wrong when creating a empty savefile')
+        else:
+            self.dataStruct = main.OpenDataStructure()  
+        sorted_totalStats = sorted(self.dataStruct['totalStats'].items(),key=lambda x:operator.getitem(x[1],'count'), reverse=True)
+        
+#        for inst in sorted_totalStats:
+#            print(inst)
+#            print(inst[0])
+#            print(inst[1]['count'])
+            
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         # Create first tab
-        self.tab1.layout = QVBoxLayout(self)
-        self.pushButton1 = QPushButton("PyQt5 button")
-        self.tab1.layout.addWidget(self.pushButton1)
-        self.tab1.setLayout(self.tab1.layout)
- 
+#        self.tab1.layout = QFormLayout(self)
+#        self.pushButton1 = QPushButton("PyQt5 button")
+#        self.pushButton2 = QPushButton("PyQt5 button")
+#        self.label1 = QLabel('Test', self)
+#        #self.tab1.layout.addWidget(self.pushButton1)
+#        #self.tab1.layout.addWidget(self.pushButton2)
+#        self.tab1.layout.addRow(self.label1, self.pushButton1)
+#        #self.tab1.layout.addRow(self.pushButton1, self.pushButton2)
+#        self.tab1.setLayout(self.tab1.layout)
+#        self.tab1.move(100, 100)
+        
+        # Create second tab
+        self.content_widget = QWidget()
+        self.tab2.setWidget(self.content_widget)
+        self.flay = QGridLayout(self.content_widget)
+        self.tab2.setWidgetResizable(True)
+        
+        self.leftColumn = QLabel('Instance Name:', self)
+        self.leftColumn.setFont(boldFont)
+        self.middleColumn = QLabel('Instance Count:', self)
+        self.middleColumn.setFont(boldFont)
+        self.rightColumn = QLabel('Avg Time in Instance(secs):', self)
+        self.rightColumn.setFont(boldFont)
+        self.flay.addWidget(self.leftColumn, 0, 0)
+        self.flay.addWidget(self.middleColumn, 0, 1)
+        self.flay.addWidget(self.rightColumn, 0, 2)
+        
+        
+        cnt = 1
+        for inst in sorted_totalStats:
+            print(inst)
+            self.labelLeft = QLabel(str(inst[0]), self)
+            self.labelMiddle = QLabel(str(inst[1]['count']), self)
+            self.labelRight = QLabel(str(inst[1]['avgTime']), self)
+            
+            self.flay.addWidget(self.labelLeft, cnt,0)
+            self.flay.addWidget(self.labelMiddle, cnt,1)
+            self.flay.addWidget(self.labelRight, cnt,2)
+            cnt += 1
+            
+            
+        
+#        self.t1 = QLabel('Test1', self)
+#        self.t2 = QLabel('Test2', self)
+#        self.t3 = QLabel('Test3', self)
+#        self.t4 = QLabel('Test4', self)
+#        self.t5 = QLabel('Test5', self)
+#        self.t6 = QLabel('Test6', self)
+#        self.flay.addRow(self.t1)
+#        self.flay.addRow(self.t2)
+#        self.flay.addRow(self.t3)
+#        self.flay.addRow(self.t4)
+#        self.flay.addRow(self.t5)
+#        self.flay.addRow(self.t6)
+        #self.tab2.setLayout(self.tab2.layout)
+        
+        
+        #Create third tab
+        self.tab3.layout = QGridLayout(self)
+        self.textbox = QLineEdit(self)
+        self.textbox.resize(50, 40)
+        self.pathLabel = QLabel('Client.txt location', self)
+        self.browseButton = QPushButton('Browse')
+        self.browseButton.clicked.connect(self.on_click)
+        
+        self.tab3.layout.addWidget(self.pathLabel, 0, 0)
+        self.tab3.layout.addWidget(self.textbox, 0, 1)
+        self.tab3.layout.addWidget(self.browseButton, 0, 2)
+        self.tab3.setLayout(self.tab3.layout)
+        
+        
+        
         # Add tabs to widget        
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
- 
+        self.tabs.resize(300,100)
+        #self.tabs.setMaximumWidth(300)
+        #self.tabs.setMaximumHeight(100)
+
+
     @pyqtSlot()
     def on_click(self):
-        print("\n")
-        for currentQTableWidgetItem in self.tableWidget.selectedItems():
-            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+        print("Button click")
+        self.t = Filedialog()
+        self.textbox.setText(self.t.path)
+
+        
+class Filedialog(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 file dialogs - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+
  
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+ 
+        self.openFileNameDialog()
+ 
+    def openFileNameDialog(self):    
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;Python Files (*.py)", options=options)
+        if fileName:
+            print(fileName)
+            self.path = fileName
+
+
+
+        
+
+
 
 #App = QApplication(sys.argv)
 App = QCoreApplication.instance()
@@ -79,3 +223,7 @@ if App is None:
     App = QApplication(sys.argv)
 window = Window()
 sys.exit(App.exec())
+
+
+
+
